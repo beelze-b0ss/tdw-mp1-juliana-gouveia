2d89ae453dcf211f3f3ae1b9dd686bbca094b1a5
Juliana Gouveia <86081801+beelze-b0ss@users.noreply.github.com>
2022-10-28 18:08:34 +0100

    snapshot

---
+++ components/Link.js
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer';
+import Link from '../Link';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Link page="http://www.facebook.com">Facebook</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
